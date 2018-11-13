00683b494190c452d82e169b45a965b38887877f
MINSK\Siarhei_Huk <siarhei_huk@epam.com>
2018-11-13 11:37:15 +0300

    destruct

--- js/components/NewsList.js
+++ js/components/NewsList.js
@@ -5,19 +5,21 @@ class NewsList extends HTMLElement {
     this.className = 'news-list';
 
     const newsElements = this.news.map(item => {
+      const { title, description } = item;
+      
       const elem = document.createElement('div');
       elem.className = 'news-list__item news-item';
       
-      const title = document.createElement('div');
-      title.className = 'news-item__title';
-      title.innerHTML = item.title;
+      const titleElement = document.createElement('div');
+      titleElement.className = 'news-item__title';
+      titleElement.innerHTML = title;
 
-      const body = document.createElement('div');
-      body.className = 'news-item__body';
-      body.innerHTML = item.description;
+      const bodyElement = document.createElement('div');
+      bodyElement.className = 'news-item__body';
+      bodyElement.innerHTML = description;
 
-      elem.appendChild(title);
-      elem.appendChild(body);
+      elem.appendChild(titleElement);
+      elem.appendChild(bodyElement);
       return elem;
     });
 
