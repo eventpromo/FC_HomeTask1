aa8f530086012af3169382fb02f765a6c60e48fd
Siarhei <eventpromoby@gmail.com>
2018-11-27 01:15:59 +0300

    dynamic import

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 import newsService from './services/QueryNewsService';
-import NewsList from './components/NewsList';
-import NewsItem from './components/NewsItem';
 import '../styles/main.scss';
-
-customElements.define('news-list', NewsList);
-customElements.define('news-item', NewsItem);
 
 document.addEventListener('DOMContentLoaded', () => {
   const mainElement = document.querySelector('main');
   const searchElement = document.querySelector('#news-query');
-  const listElement = new NewsList({});
-  listElement.className += ' main__item';
-  mainElement.appendChild(listElement);
 
   searchElement.addEventListener('change', async (ev) => {
+    let listElement = document.querySelector('news-list');
+    if (!listElement) {
+      const NewsList = (await import('./components/NewsList')).default;
+      const NewsItem = (await import('./components/NewsItem')).default;
+
+      customElements.define('news-list', NewsList);
+      customElements.define('news-item', NewsItem);
+
+      listElement = new NewsList({});
+      listElement.className += ' main__item';
+      mainElement.appendChild(listElement);
+    }
+
     if (ev.target.value) {
       const data = await newsService.read(ev.target.value);
       listElement.items = data.articles;
