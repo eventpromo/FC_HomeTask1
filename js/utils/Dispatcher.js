a8b963ce11170a4ce15e28da33d7c7e4906bc93d
Siarhei <eventpromoby@gmail.com>
2018-12-06 02:58:58 +0300

    build and fix

--- js/utils/Dispatcher.js
+++ js/utils/Dispatcher.js
@@ -1,11 +1,9 @@
-import Observer from './Observer';
 import singletonDecorator from '../decorators/singletonDecorator';
 import getNews from '../actions/getNews';
 import updateListNews from '../actions/updateListNews';
 
-class Dispatcher extends Observer {
+class Dispatcher {
   constructor() {
-    super();
     this.getNews = getNews;
     this.updateListNews = updateListNews;
   }
